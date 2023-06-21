fe17f4f5f25e55bd188c3d3df0569b71ef7dafa7
mstepan <maksym.stepanenko@oracle.com>
2023-06-21 09:45:42 +0300

    Small chnages.

--- src/main/java/com/max/app/Main.java
+++ src/main/java/com/max/app/Main.java
@@ -1,21 +1,10 @@
 package com.max.app;
 
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.util.Date;
-
 public final class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
 
-        Date oldDate = new Date();
-        System.out.println(oldDate);
-
-        LocalDateTime date = LocalDateTime.now();
-        System.out.println(date.getHour());
-
-        System.out.printf("java version: %s%n", System.getProperty("java.version"));
+        System.out.printf(
+                "Main completed... java version: %s%n", System.getProperty("java.version"));
     }
-
-
 }
